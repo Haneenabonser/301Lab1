4e360b2858eda05b77743629db73ed0aea836c8e
Haneenabonser <aabonser@gmail.com>
2021-05-18 23:52:49 +0300

    lab3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import Main from './components/Main.js';
 
-
 class App extends React.Component {
+
+  // constructor (props){
+  //   super(props);
+  //   this.state({
+  //     dataArrar : data
+  //   })
+  // }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      show : false
+    }
+  }
+
+
+
   render(){
 
 
     return(
       <>
       <Main/>
+
+
 
       </>
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,70 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
-import data from './data.json'
+import data from './data.json';
+import SelectedBeast from './SelectedBeast'
 
 
 class Main extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: data,
+            show: false
+        }
+    }
+
+
+    // showModel = () => {
+    showModal = (active) => {
+        this.setState({
+            show: active
+        })
+
+    }
+
+
+
+    //  Example() {
+    //     const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
     render() {
 
 
         return (
             <>
-            <h1>Gallery of Hornes</h1>
-            {data.map(item=>{
-                return (
-                    <HornedBeasts
-                title = {item.title}
-                imgURL = {item.image_url}
-                description = {item.description}
-                />
-                )
-            })}
+                <h1>Gallery of Hornes</h1>
+
+                {data.map(item => {
+                    return (
+                        <HornedBeasts
+                            title={item.title}
+                            imgURL={item.image_url}
+                            description={item.description}
+                        />
+                    )
+                })
+                }
+                {
+
+                    <SelectedBeast
+                        beastArr={this.state.horndBeastData}
+                        // showModel={this.showModel}
+                        handleClose={this.handleClose}
+                        handleShow={this.handleShow}
+                        show={this.state.show}
+
+                    />
+                }
             </>
 
         )
-    }};
+    }
+};
 
 
-    export default Main;
+export default Main;
